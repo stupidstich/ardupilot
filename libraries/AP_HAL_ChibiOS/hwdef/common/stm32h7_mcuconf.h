ef6fee161758ab3a64755f8526df58739c58c666
stupidstich <lincoln@lincolnbarnes.me.uk>
2020-01-01 18:27:16 +0000

    Update stm32h7_mcuconf.h

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
@@ -216,9 +216,9 @@
 #define STM32_ADC_ADC3_CLOCK_MODE           ADC_CCR_CKMODE_ADCCK
 
 // we call it ADC1 in hwdef.dat, but driver uses ADC12 for DMA stream
-#ifdef STM32_ADC_ADC1_DMA_STREAM
+//#ifdef STM32_ADC_ADC1_DMA_STREAM
 #define STM32_ADC_ADC12_DMA_STREAM STM32_ADC_ADC1_DMA_STREAM
-#endif
+//#endif
 
 /*
  * CAN driver system settings.
